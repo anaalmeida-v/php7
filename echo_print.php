670edadb650a10003ab1911dcfa87987da93934c
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-25 14:48:46 -0300

    saída de dados com echo() e print()

---
+++ echo_print.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+
+<body>
+    <?php
+    echo 'Comando echo<br/>'; //construtor da linguagem
+    print "Comando print<hr/>"; //inicialmente era uma função(que pode ser utilizada normalmente)
+    
+    //quando função print é executada corretamente, retorna um, quando há erro, retorna 0
+    echo print("comando print<hr/>")//result=1
+
+        //os dois tem o mesmo objetivo
+        //impressão de conteúdo vindo docódigo php na página html
+        ?>
+</body>
+
+</html>
