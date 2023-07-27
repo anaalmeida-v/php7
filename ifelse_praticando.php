a14afeb276133003c16ccb89daae08fa3e4912b5
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-27 14:55:39 -0300

    operador ternário

--- ifelse_praticando.php
+++ ifelse_praticando.php
@@ -13,6 +13,10 @@
     $valor_frete = 50;
     $recebeu_desconto_frete = true;
 
+    $valor_frete_aux = $usuariopossui_cartaoloja && $valor_compra >= 400 ? 0 : ($usuariopossui_cartaoloja >= 300 ? 10 : ($usuariopossui_cartaoloja && $valor_compra >= 100 ? 25 : $valor_frete));
+    $recebeu_desconto_frete = $valor_frete != $valor_frete_aux ? true : false;
+    $valor_frete = $valor_frete_aux;
+    /*
     if ($usuariopossui_cartaoloja && $valor_compra >= 400) {
         $valor_frete = 0;
     } else if ($usuariopossui_cartaoloja && $valor_compra >= 300) {
@@ -22,17 +26,20 @@
     } else {
         $recebeu_desconto_frete = false;
     }
+    */
     ?>
 
     <h1>Detalhes do pedido</h1>
     <p>Possui cartão da loja?
-        <?php
-        if ($usuariopossui_cartaoloja) {
-            echo 'SIM';
-        } else {
-            echo 'NÃO';
-        }
-        ?>
+        <?= $usuariopossui_cartaoloja ? 'SIM' : 'NÃO' ?>
+        <? /*
+//<condicao> ?true:false
+/*if ($usuariopossui_cartaoloja) {
+echo 'SIM';
+} else {
+echo 'NÃO';
+}
+*/?>
     </p>
 
     <p>Valor da compra:
@@ -40,13 +47,7 @@
     </p>
 
     <p>Recebeu desconto no frete?
-        <?php
-        if ($recebeu_desconto_frete) {
-            echo 'SIM';
-        } else {
-            echo 'NÃO';
-        }
-        ?>
+        <?= $recebeu_desconto_frete ? 'SIM' : 'NÃO' ?>
     </p>
 
     <p>Valor do frete:
