2ed2ba2443c9dba2e663c49eaae5c893d7defdc7
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-27 11:27:53 -0300

    introd. ifelse + op. de comparação + prática

---
+++ ifelse_operadorescondicionais.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    //== 
+    //===
+    //!= ou <>
+    //!==
+    //identico e nao identico verifica valores e tipos
+    //<
+    //>
+    //<=
+    //>=
+    
+    //caso haja apenas uma condição de if, chave não é necessária
+    if (1 >= 2) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+    ?>
+</body>
+
+</html>
