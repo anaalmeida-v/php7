b4eb3d528afdd09d6bcef60109d576cd1dfc6568
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-27 17:32:38 -0300

    operadores aritméticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php
+        $num1=13;
+        $num2=4;
+        
+        echo "A soma entre $num1 e $num2, é: ".($num1+$num2);
+        echo'<br/>';
+        echo "A subtração entre $num1 e $num2, é: ".($num1-$num2);
+        echo'<br/>';
+        echo "A multiplicação entre $num1 e $num2, é: ".($num1*$num2);
+        echo'<br/>';
+        echo "A divisão entre $num1 e $num2, é: ".($num1/$num2);
+        echo'<br/>';
+        echo "A módulo entre $num1 e $num2, é: ".($num1%$num2);
+        echo'<br/>';
+        ?>
+</body>
+</html>
