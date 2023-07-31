bf01c8c024fb13cf8cabfb331d32cd762d886244
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-31 17:05:49 -0300

    for

---
+++ loops_for.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    /* $x=1;
+    while($x<10) {
+
+    }
+
+    do{
+
+    } while();
+    */
+
+    for ($x = 10; $x > 0; $x--) { //ordem é fundamental
+    
+        //break
+        //continue
+        echo "$x <br/>";
+    }
+    ?>
+</body>
+
+</html>
