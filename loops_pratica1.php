b3b4246db9521d3441c1456bf8a2ab0d6dbe877a
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-07-31 17:41:32 -0300

    percorrendo arrays -> while, do while e for

--- loops_pratica1.php
+++ loops_pratica1.php
@@ -8,6 +8,23 @@
 
 <body>
     <?php
+    $registros = [
+        ['título' => 'Título notícia 3', 'conteúdo' => 'Conteúdo notícia 1'],
+        ['título' => 'Título notícia 2', 'conteúdo' => 'Conteúdo notícia 2'],
+        ['título' => 'Título notícia 3', 'conteúdo' => 'Conteúdo notícia 3'],
+        ['título' => 'Título notícia 4', 'conteúdo' => 'Conteúdo notícia 4'],
+    ];
+
+    echo '<pre>';
+    //print_r($registros);
+    echo '</pre>';
+    echo '<br/><br/><br/>';
+    $idx = 0;
+
+    //count -> conta qtd de elementos de array
+    echo 'O array possui: ' . count($registros) . ' registros';
+    echo '<br/>';
+    
 
     ?>
 </body>
