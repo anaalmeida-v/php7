836ebd26fe4bc343497e2009ff0fe29b381edd70
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-01 13:51:09 -0300

    foreach_finaliz.

--- loops_foreach.php
+++ loops_foreach.php
@@ -13,11 +13,12 @@
     print_r($itens);
     echo '</pre>';
 
-    foreach ($itens as $item) {
-        echo "$item <br/>";
+    foreach ($itens as $item) {//chaves encapsulam o conteúdo que será executado para cada um dos elementos dentro do array
+        //array + as + definicao de variável que receberá valor contido em cada item do array
+        echo "$item ";
 
         if ($item == 'mesa') {
-            echo '(*Compre uma mesa e ganhe 25% de desconto na compra de 4 cadeiras)';
+            echo ' (*Compre uma mesa e ganhe 25% de desconto na compra de 4 cadeiras)';
         }
         echo '<br/>';
     }
