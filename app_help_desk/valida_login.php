1c74417f50c491fe84c7ef29bfac698f0f05b241
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-02 14:44:16 -0300

    formularios(get e post)

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,5 +1,13 @@
 <?php
-print_r($_GET);
+/*print_r($_GET); //get - resgata dados
+echo '<br/>';
 $_GET['email'];
-$_GET['senha'];
+echo '<br/>';
+$_GET['senha'];*/
+
+print_r($_POST); //POST - posta dados (usuário consegue visualizar)
+echo '<br/>';
+$_POST['email'];
+echo '<br/>';
+$_POST['senha'];
 ?>
