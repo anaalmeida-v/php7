b071325f1fa1bd6aab8c9e6eea7eefcf07a40ec6
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-03 14:44:42 -0300

    include, require, _once...

---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,11 @@
+<?php
+include("menu.php"); //warning
+require "menu2.php"; //fatal error
+?>
+conteúdo da página (início)
+<br />
+
+<?php
+include_once("menu.php"); //warning
+require_once "menu2.php"; //fatal error
+?>
