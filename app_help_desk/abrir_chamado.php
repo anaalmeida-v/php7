97c1f51ad85a39c50638cc193841a98c598605c7
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-03 15:29:53 -0300

    navegação entre páginas

--- app_help_desk/abrir_chamado.php
+++ app_help_desk/abrir_chamado.php
@@ -1,6 +1,7 @@
 <?php require_once "validador_acesso.php" ?>
 
 <html>
+
 <head>
   <meta charset="utf-8" />
   <title>App Help Desk</title>
@@ -62,11 +63,11 @@
 
                   <div class="row mt-5">
                     <div class="col-6">
-                      <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                      <a class="btn btn-lg btn-warning btn-block" href="home.php" type="submit">Voltar</a>
                     </div>
 
                     <div class="col-6">
-                      <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+                      <a class="btn btn-lg btn-info btn-block" href="" type="submit">Abrir</a>
                     </div>
                   </div>
                 </form>
--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); //quando se trabalha com sessão é fundamental executar essa função, antes de qualquer instrução que emita uma saída de dados no navegador
-   
+
 //variável verifica se a autenticação foi realizada
 $usuarios_autenticado = false; //false pois vira true apenas se entrar na condição verdadeira
 //usuários do sistema
@@ -21,6 +21,7 @@ foreach ($usuarios_app as $user) {
 if ($usuarios_autenticado) {
     echo 'Usuário autenticado';
     $_SESSION['autenticado'] = 'SIM';
+    header('Location: home.php'); //haverá um encaminhamento do usuário; destino: home
 } else {
     $_SESSION['autenticado'] = 'NÃO';
     header('Location: index.php?login=erro'); //haverá um encaminhamento do usuário; destino: página inicial
