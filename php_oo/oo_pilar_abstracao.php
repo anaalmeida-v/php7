9df59c348bd504af9fd38a529ae0d2f7cee0280c
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-09 14:31:55 -0300

    getters e setters

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -11,6 +11,23 @@ class Funcionario
     public $numFilhos = null;
 
     //getters e setters
+    function setNome($nome)
+    {
+        $this->nome = $nome;
+    } //para manipular um atributo do próprio objeto basta utilizar a palavra reservada this e atribui-se a ela o valor passado por parâmetro na função
+    function setNumFilhos($numFilhos)
+    {
+        $this->numFilhos = $numFilhos;
+    }
+
+    function getNome()
+    {
+        return $this->nome;
+    }
+    function getNumFilhos()
+    {
+        return $this->numFilhos;
+    }
 
     //métodos - na prática são funções
     //padrão de definição de nomes segue mesma regra da definição de variáveis-atributos
@@ -28,21 +45,25 @@ class Funcionario
 }
 //variável recebe instância da classe Funcionário
 $y = new Funcionario();
-
+$y->setNome('Jose');
+$y->setNumFilhos(2);
+echo $y->resumirCadFunc();
 //criando objeto com base no modelo Funcionário e atribuindo ele a variável $y
 //a partir dessa variável estamos recebendo os atributos e métodos do objeto
+echo '<br/>';
+echo $y->getNome() . ' possui ' . $y->getNumFilhos() . ' filho(s) ';
 
 //acessando atributos ou métodos(->):
-echo $y->resumirCadFunc(); //exibindo resumo do cadastro do funcionario
-echo '<br/>';
-$y->modificarNumFilhos(3); //modificando num de filhos
-echo $y->resumirCadFunc(); //exibindo  cadastro novamente (com informação de numFilhos atualizada)
+//echo $y->resumirCadFunc(); //exibindo resumo do cadastro do funcionario
+//$y->modificarNumFilhos(3); //modificando num de filhos
+//echo $y->resumirCadFunc(); //exibindo  cadastro novamente (com informação de numFilhos atualizada)
 echo '<hr/>';
 
 //nova instância de objeto / novo funcionário
 $x = new Funcionario();
-echo $x->resumirCadFunc();
-echo '<br/>';
-echo $x->modificarNumFilhos(1);
-echo $x->resumirCadFunc();
+$x->setNome('Maria');
+$x->setNumFilhos(0);
+echo $x->getNome() . ' possui ' . $x->getNumFilhos() . ' filho(s) ';
+
+
 ?>
