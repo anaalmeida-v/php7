6dabfc9d79fe05d1649eadf2d4b9b10a94bf2c94
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-09 15:27:11 -0300

    getters e setters mágicos

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -9,9 +9,21 @@ class Funcionario
     public $nome = null;
     public $telefone = null;
     public $numFilhos = null;
+    public $cargo = null;
+    public $salario = null;
+    //getters e setters (overloading / sobrecarga)
+    function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
 
-    //getters e setters
-    function setNome($nome)
+    function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+
+    /*function setNome($nome)
     {
         $this->nome = $nome;
     } //para manipular um atributo do próprio objeto basta utilizar a palavra reservada this e atribui-se a ela o valor passado por parâmetro na função
@@ -27,7 +39,7 @@ class Funcionario
     function getNumFilhos()
     {
         return $this->numFilhos;
-    }
+    }*/
 
     //métodos - na prática são funções
     //padrão de definição de nomes segue mesma regra da definição de variáveis-atributos
@@ -45,13 +57,13 @@ class Funcionario
 }
 //variável recebe instância da classe Funcionário
 $y = new Funcionario();
-$y->setNome('Jose');
-$y->setNumFilhos(2);
+$y->__set('nome', 'Jose');
+$y->__set('numFilhos', 2);
 echo $y->resumirCadFunc();
 //criando objeto com base no modelo Funcionário e atribuindo ele a variável $y
 //a partir dessa variável estamos recebendo os atributos e métodos do objeto
 echo '<br/>';
-echo $y->getNome() . ' possui ' . $y->getNumFilhos() . ' filho(s) ';
+echo $y->__get('nome') . ' possui ' . $y->__get('numFilhos') . ' filho(s) ';
 
 //acessando atributos ou métodos(->):
 //echo $y->resumirCadFunc(); //exibindo resumo do cadastro do funcionario
@@ -61,9 +73,9 @@ echo '<hr/>';
 
 //nova instância de objeto / novo funcionário
 $x = new Funcionario();
-$x->setNome('Maria');
-$x->setNumFilhos(0);
-echo $x->getNome() . ' possui ' . $x->getNumFilhos() . ' filho(s) ';
+$x->__set('nome', 'Maria');
+$x->__set('numFilhos', 0);
+echo $x->__get('nome') . ' possui ' . $x->__get('numFilhos') . ' filho(s) ';
 
 
 ?>
