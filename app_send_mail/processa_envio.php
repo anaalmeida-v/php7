b23910604bc2852b114556aed26ef630891073c6
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-17 16:11:04 -0300

    ajuste de encaminhamento require

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -1,11 +1,11 @@
 <?php
 //importando biblioteca PHPMailer
-require "../../app_send_mail/bibliotecas/PHPMailer/Exception.php";
-require "../../app_send_mail/bibliotecas/PHPMailer/OAuthTokenProvider.php";
-require "../../app_send_mail/bibliotecas/PHPMailer/OAuth.php";
-require "../../app_send_mail/bibliotecas/PHPMailer/PHPMailer.php";
-require "../../app_send_mail/bibliotecas/PHPMailer/POP3.php"; //-->especificações do protocolo de recebimento de email
-require "../../app_send_mail/bibliotecas/PHPMailer/SMTP.php"; //-->especificações do protocolo de envio de email
+require "../../../app_send_mail/bibliotecas/PHPMailer/Exception.php";
+require "../../../app_send_mail/bibliotecas/PHPMailer/OAuthTokenProvider.php";
+require "../../../app_send_mail/bibliotecas/PHPMailer/OAuth.php";
+require "../../../app_send_mail/bibliotecas/PHPMailer/PHPMailer.php";
+require "../../../app_send_mail/bibliotecas/PHPMailer/POP3.php"; //-->especificações do protocolo de recebimento de email
+require "../../../app_send_mail/bibliotecas/PHPMailer/SMTP.php"; //-->especificações do protocolo de envio de email
 
-require "../../app_send_mail/processa_envio.php";
+require "../../../app_send_mail/processa_envio.php";
 ?>
