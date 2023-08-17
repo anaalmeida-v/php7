f23de0a9b54aaaeff221a2c109ead3368b19d756
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-08-17 11:42:33 -0300

    Melhorando o feedback visual - success / error

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -21,6 +21,7 @@ class Mensagem
     private $para = null;
     private $assunto = null;
     private $mensagem = null;
+    public $status = ['codigo_status' => null, 'descricao_status' => ''];
     public function __get($atributo)
     {
         return $this->$atributo;
@@ -48,7 +49,7 @@ $mensagem->__set('mensagem', $_POST['mensagem']);
 
 //print_r($mensagem)
 if (!$mensagem->mensagemValida()) { //se mensagem não for válida (pelo menos um dos campos não estiverem preenchidos) -->entra no if
-    echo 'Mensagem é válida';
+    //echo 'Mensagem é válida';
     //die(); //mata processamento do script no ponto em que a instrução é lida
     header('Location: index.php'); //redirecionando usuário(caso acesso seja inválido) para página inicial do app
 }
@@ -58,7 +59,7 @@ $mail = new PHPMailer(true); //produz instância de PHPMailer
 //codificacao da biblioteca phpmailer
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+    $mail->SMTPDebug = false; //Enable verbose debug output
     $mail->isSMTP(); //Send using SMTP
     $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
     $mail->SMTPAuth = true; //Enable SMTP authentication
@@ -70,24 +71,75 @@ try {
     //Recipients
     $mail->setFrom('testeanaalmeida@gmail.com', 'Ana Almeida Remetente'); //remetente do email (quem o envia)
     $mail->addAddress($mensagem->__get('para')); //destinatário do email (quem o recebe)
-    //$mail->addReplyTo('info@example.com', 'Information');//contato padrão caso o destiatário deseje responder o remetente
-    //$mail->addCC('cc@example.com');//adiciona destinatário de cópias
-    //$mail->addBCC('bcc@example.com');//cópia oculta
+//$mail->addReplyTo('info@example.com', 'Information');//contato padrão caso o destiatário deseje responder o remetente
+//$mail->addCC('cc@example.com');//adiciona destinatário de cópias
+//$mail->addBCC('bcc@example.com');//cópia oculta
 
     //Attachments - possibilidade de adicionar anexo ao email
-    //$mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
+//$mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
 
     //Content
     $mail->isHTML(true); //Set email format to HTML
     $mail->Subject = $mensagem->__get('assunto'); //assunto do email
     $mail->Body = $mensagem->__get('mensagem'); //conteúdo
-    $mail->AltBody = 'É necessário  utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
+    $mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso total ao conteúdo dessa mensagem';
+
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
 
     $mail->send();
-    echo 'Email enviado com sucesso';
+    $mensagem->status['codigo_status'] = 1;
+    $mensagem->status['descricao_status'] = 'Email enviado com sucesso';
 } catch (Exception $e) {
-    echo "Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde.";
-    echo 'Detalhes do erro: ' . $mail->ErrorInfo;
+    $mensagem->status['codigo_status'] = 2;
+    $mensagem->status['descricao_status'] = 'Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde. Detalhes do erro: ' . $mail->ErrorInfo;
+    //lógica que armazene o erro para posterior análise por parte do programador
 }
 ?>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>App Mail Send</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="py-3 text-center">
+            <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+            <h2>Send Mail</h2>
+            <p class="lead">Seu app de envio de e-mails particular!</p>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                if ($mensagem->status['codigo_status'] == 1) { ?>
+                    <div class="container">
+                        <h1 class="display-4 text-success">Sucesso</h1>
+                        <p>
+                            <?= $mensagem->status['descricao_status'] ?>
+                        </p>
+                        <div>
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                        </div>
+                    </div>
+                <?php } ?>
+                <?php
+                if ($mensagem->status['codigo_status'] == 2) { ?>
+                    <div class="container">
+                        <h1 class="display-4 text-danger">Ops!</h1>
+                        <p>
+                            <?= $mensagem->status['descricao_status'] ?>
+                        </p>
+                        <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
